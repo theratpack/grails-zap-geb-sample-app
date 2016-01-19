b178c3d12f3364279402a4cde88b9b6a0bc27b78
Rafael Luque <rafael.luque@osoco.es>
2016-01-19 16:42:40 +0100

    Fix application's zap security config to use default values

--- grails-app/conf/ZapSecurityTestsConfig.groovy
+++ grails-app/conf/ZapSecurityTestsConfig.groovy
@@ -1,8 +1,8 @@
-import org.zaproxy.clientapi.core.Alert
+//import org.zaproxy.clientapi.core.Alert
 
 zap {
     // Absolute path where ZAP is installed
-    installDir = '/home/chous/toolbox/ZAP'
+    installDir = '/opt/zaproxy'
 
     // Address the proxy will bind
     proxyHost = 'localhost'
@@ -47,5 +47,5 @@ zap {
     timeout = 10000
 
     // Enable debug in the REST based API interactions with ZAP
-    debug = true
+    debug = false
 }
